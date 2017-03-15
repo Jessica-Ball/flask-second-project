fddf6aeae177af6c86e9f336db85abb85d90b58d
Jessica Ball <jessica.ball@landregistry.gov.uk>
2017-03-15 08:55:22 +0000

    2nd commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
